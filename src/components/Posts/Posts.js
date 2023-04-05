2b26d0ea789ec129e405a10934c3fca7a090c5e6
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-05 13:44:57 +0300

    Компонента Posts - презентационная Компонента PostsContainer - контейнерная из 2х уровней, первый делает запрос на сервер, второй получаем пропсы через connect

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,18 +1,31 @@
 import React from "react";
 import './Posts.css'
-import axios from "axios";
 
 const Posts = (props) => {
-    if (props.posts.length === 0) {
-        axios.get('http://127.0.0.1:5000/posts').then(response => {
-            let posts = response.data.posts;
-            props.setPosts(posts);
-        });
+    let pagesCount = Math.ceil(props.count / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
     return (
         <div className={'Posts'}>
             <div className={'body'}>
+                <div>
+                    {pages.map(page => {
+                        return <span
+                            onClick={() => {
+                                props.setCurrentPage(page);
+                            }}
+                            key={page}
+                            className={
+                                (props.currentPage === 0 ? 1 : props.currentPage) === page ? 'select' : 'notselect'
+                            }
+                        >{page}</span>
+                    })}
+                </div>
                 <div>Posts</div>
+                <hr/>
+                <div>Count: {props.count}</div>
                 <hr/>
                 {props.posts.map(post => <div key={post.id}>
                     <span>{post.nameUser}</span>
--- src/components/Posts/PostsContainer.js
+++ src/components/Posts/PostsContainer.js
@@ -1,6 +1,52 @@
 import {connect} from "react-redux";
-import PostsC from "./PostsC";
 import {setCount, setCurrentPage, setUsers} from "../../redux/reducers/postsReducer";
+import React from "react";
+import axios from "axios";
+import Posts from "./Posts";
+
+class PostsContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.setCurrentPage = this.setCurrentPage.bind(this);
+    }
+
+    componentDidMount() {
+        console.log(this.props.currentPage)
+        if (this.props.posts.length === 0) {
+            axios.get(`http://127.0.0.1:5000/posts?page=${this.props.currentPage}`)
+                .then(response => {
+                let posts = response.data.posts;
+                let count = response.data.count;
+                this.props.setPosts(posts);
+                this.props.setCount(count);
+            });
+        }
+    }
+
+    setCurrentPage(currentPage) {
+        // обработка ошибки СORS при передаче гет параметра page=1
+        if (currentPage === 1) {
+            currentPage = 0;
+        }
+        this.props.setCurrentPage(currentPage);
+        axios.get(`http://127.0.0.1:5000/posts?page=${currentPage}`).then(response => {
+            let posts = response.data.posts;
+            this.props.setPosts(posts);
+        });
+    }
+
+    render() {
+        return <Posts
+            posts={this.props.posts}
+            pages={this.props.pages}
+            count={this.props.count}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            setCurrentPage={this.setCurrentPage} // ????
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -25,6 +71,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const PostsContainer = connect(mapStateToProps, mapDispatchToProps)(PostsC);
-
-export default PostsContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
