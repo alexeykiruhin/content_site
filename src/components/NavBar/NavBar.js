060ee471cc4c72abb87535dfe49ac2e296ee8244
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-11 01:15:08 +0300

    Изменения в меню

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './NavBar.css';
 import {NavLink} from "react-router-dom";
 
-const NavBar = (props) => {
+const NavBar = () => {
     return (
         <div className={'NavBar'}>
             <div className={'wrapperNavBar'}>
                 <h3>Menu</h3>
-                <NavLink className={'item'} to="/">Posts</NavLink>
-                <NavLink className={'item'} to="/create">Create</NavLink>
+                <NavLink className={'item'} to="/">Home</NavLink>
                 <NavLink className={'item'} to="/users">Users</NavLink>
+                <NavLink className={'item'} to="/create">Create</NavLink>
             </div>
         </div>
     )
