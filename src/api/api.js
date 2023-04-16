2671c36bfee83654b41ea505b31221b028681960
alexeykiryukhin <q23sa3501080@gmail.com>
2023-04-17 01:09:15 +0300

    Получение токена в локалсторедж и отправку на серв

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,20 @@
 import axios from "axios";
+import jwt_decode from 'jwt-decode';
 // import Cookies from 'js-cookie';
 
 
 const instance = axios.create({
     withCredentials: true,
-    baseURL: 'http://127.0.0.1:5000/'
+    baseURL: 'http://127.0.0.1:5000/api/'
 });
 
-let token = null;
-console.log(token)
-if (token) {
-    instance.defaults.headers.common['Authorization'] = token;
+
+const token = localStorage.getItem('token');
+console.log(token);
+if(token){
+    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
+
 
 export const API = {
 
@@ -33,8 +36,12 @@ export const API = {
     },
 
     getPosts(currentPage) {
-        return instance.get(`posts?page=${currentPage}&page_size=2`)
-            .then((response) => response.data)
+        const token = localStorage.getItem('token');
+        // console.log(token);
+        return instance.get(`posts?page=${currentPage}&page_size=2`, { headers: { Authorization: `Bearer ${token}` }})
+            .then((response) => {
+                return response.data
+            })
     },
 
     login(username, password) {
@@ -42,10 +49,16 @@ export const API = {
             username: username,
             password: password
         }).then((response) => {
-            token = response.headers['access-token'];
-            instance.defaults.headers.common['Authorization'] = token;
-            console.log(response.data)
+            if (response.data) {
+                const token = response.data.access_token; // запись токена
+                localStorage.setItem('token', token); // сохранение токена в localStorage
+                instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                // console.log(token);
+            }
             return response.data
         })
     },
 }
+// продолжить с авторизацией, сейчас токен получаю, записываю в локал сторедж, отправляю с запросами на сервер в заголовках токен
+// добавить логаут и переписать логику кнопки логинв хедере
+// добавить регистрацию, перенести авторизацию из хедера в апп
